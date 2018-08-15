1eb6ba17e345e26f49f145d94b2a0621fc90a0f1
max <daehwanchoi@gmail.com>
2018-08-16 01:13:00 +0900

    springboot 버전 변경, 실행 오류 수정 및 개선

--- src/main/java/com/olio/aglio/market/MarketWasApplication.java
+++ src/main/java/com/olio/aglio/market/MarketWasApplication.java
@@ -3,17 +3,17 @@ package com.olio.aglio.market;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class MarketWasApplication extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return super.configure(builder);
+        return builder.sources(MarketWasApplication.class);
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(MarketApplication.class, args);
+        SpringApplication.run(MarketWasApplication.class, args);
     }
 }
